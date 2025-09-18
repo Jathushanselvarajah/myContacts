b5f61054e741071fb17f47f71e8127f07f75a372
Jathushan Selvarajah <126668649+Jathushanselvarajah@users.noreply.github.com>
2025-09-18 16:13:36 +0200

    feat(swagger): ajout de la documentation API avec swagger-jsdoc et swagger-ui-express

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 const express = require("express");
+const swaggerUi = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
 const mongoose = require("mongoose");
 const app = express();
 const MONGO_URI = process.env.MONGO_URI;
@@ -9,6 +11,26 @@ mongoose
   .connect(MONGO_URI)
   .then(() => console.log("Connecté à MongoDB Atlas"))
   .catch((err) => console.error("Erreur MongoDB :", err));
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: "myContacts",
+      version: "1.0.0",
+      description: "Swagger pour myContacts",
+    },
+    servers: [
+      {
+        url: "http://localhost:5000",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use(express.json());
 const authRoutes = require("./routes/auth");
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,6 +2,62 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
 
+/**
+ * @swagger
+ * /auth/register:
+ *   post:
+ *     summary: Crée un nouvel utilisateur
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 example: test@example.com
+ *               password:
+ *                 type: string
+ *                 example: motdepasse
+ *     responses:
+ *       201:
+ *         description: Utilisateur créé avec succès
+ *       409:
+ *         description: Email déjà utilisé
+ *       500:
+ *         description: Erreur serveur
+ */
+
+/**
+ * @swagger
+ * /auth/login:
+ *   post:
+ *     summary: Connecte un utilisateur
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 example: test@example.com
+ *               password:
+ *                 type: string
+ *                 example: motdepasse
+ *     responses:
+ *       200:
+ *         description: Connexion réussie
+ *       401:
+ *         description: Utilisateur non trouvé ou mot de passe incorrect
+ *       500:
+ *         description: Erreur serveur
+ */
+
 router.post("/register", authController.register);
 router.post("/login", authController.login);
 
